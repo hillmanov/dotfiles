5d747a135d4dc959fd9f9e68b4a82cbdcbd1ea8c
Scott Hillman <hillmanov@gmail.com>
2022-11-07 13:53:01 -0700

    Get UltiSnips to work in all JS files

--- nvim/lua/plugins/config/ultisnips.lua
+++ nvim/lua/plugins/config/ultisnips.lua
@@ -5,3 +5,8 @@ g.UltiSnipsExpandTrigger = ';;'
 g.UltiSnipsJumpForwardTrigger = ';;'
 g.UltiSnipsJumpBackwardTrigger = '::'
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'javascript,javascriptreact,typescript,typescriptreact',
+  command = 'UltiSnipsAddFiletypes javascript.javascriptreact.typescript.typescriptreact'
+})
+
