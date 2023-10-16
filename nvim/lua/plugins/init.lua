6e90a8aae0657d02754fa5016c36754ddcb79341
Scott Hillman <hillmanov@gmail.com>
2023-10-16 11:42:42 -0600

    Took noice out. No thanks

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -84,56 +84,6 @@ require('lazy').setup({
   "nvim-telescope/telescope.nvim",
   "github/copilot.vim",
   "muniftanjim/nui.nvim",
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      routes = {
-        {
-          filter = {
-            event = "msg_show",
-            any = {
-              { find = "%d+L, %d+B" },
-              { find = "; after #%d+" },
-              { find = "; before #%d+" },
-            },
-          },
-          view = "mini",
-        },
-        {
-          filter = {
-            event = "msg_show",
-            kind = "",
-            find = "written",
-          },
-          opts = { skip = true },
-        },
-      },
-      views = {
-        cmdline_popup = {
-          border = {
-            style = "none",
-            padding = { 1, 1 },
-          },
-          filter_options = {},
-          win_options = {
-            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-          },
-        },
-      },
-    },
-    dependencies = {
-      "muniftanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    }
-  },
   "isobit/vim-caddyfile",
 })
 
